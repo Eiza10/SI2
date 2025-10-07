a31ee19ef8e130afd2e6333eede0c7024804335e
Haritz Eizagirre <hartz2004@gmail.com>
2025-10-07 15:46:45 +0200

    CI probatzen

--- Rides24/src/main/java/dataAccess/DataAccess.java
+++ Rides24/src/main/java/dataAccess/DataAccess.java
@@ -8,12 +8,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
-
 import configuration.ConfigXML;
 import configuration.UtilDate;
 import domain.*;
